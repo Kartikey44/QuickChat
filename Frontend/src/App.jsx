3e63fb69acbd0fd6b3ef1eafda27dc5f185cf79c
kartikey@44 <saraswatkartikey842@gmail.com>
2026-05-28 01:22:56 +0530

    feat: improve chat UX with replies, editing, deletion, chat sorting and optimized auth loading

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Loader from "./components/Sidebar/Loader";
+
 import { useAuth } from "./context/AuthContext";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import ChatPage from "./pages/ChatPage";
@@ -14,34 +16,42 @@ import Landing from "./components/Landing";
 function App() {
   const { authUser, isCheckingAuth } = useAuth();
 
-  if (isCheckingAuth) {
-    return <Loader type="auth" fullScreen={true} />;
-  }
-
   return (
     <div className="min-h-screen w-screen">
-      <Routes>
-        <Route path="*" element={<NotFound />} />
+      {/* OPTIONAL TOP LOADER */}
+      {isCheckingAuth && (
+        <div className="fixed top-0 left-0 z-[9999] w-full">
+          <Loader type="line" fullScreen={false} />
+        </div>
+      )}
 
+      <Routes>
+        {/* CHAT */}
         <Route
           path="/chat/:user_id?"
           element={authUser ? <ChatPage /> : <Navigate to="/" />}
         />
 
+        {/* SIGNUP */}
         <Route
           path="/signup"
-          element={!authUser ? <Signup /> : <Navigate to="/chat" />}
+          element={authUser ? <Navigate to="/chat" /> : <Signup />}
         />
 
+        {/* LOGIN */}
         <Route
           path="/login"
-          element={!authUser ? <Login /> : <Navigate to="/chat" />}
+          element={authUser ? <Navigate to="/chat" /> : <Login />}
         />
 
+        {/* LANDING */}
         <Route
           path="/"
-          element={!authUser ? <Landing /> : <Navigate to="/chat" />}
+          element={authUser ? <Navigate to="/chat" /> : <Landing />}
         />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Toaster position="top-center" />
